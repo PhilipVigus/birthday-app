5761364a051809317dffff615d11abc562130ea5
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-01 12:03:47 +0100

    Refactor homepage form tests

--- spec/features/birthday_form_spec.rb
+++ spec/features/birthday_form_spec.rb
@@ -1,26 +1,24 @@
 feature 'displays the form for the user to enter their birthday into' do
-  scenario 'homepage has correct message on it' do
+  before do
     visit('/')
+  end
+  scenario 'homepage has correct message on it' do
     expect(page).to have_content('Hello there!')
   end
 
   scenario 'homepage has a field to enter your name' do
-    visit('/')
     expect(page).to have_field('name')
   end
 
   scenario 'homepage has a field to enter your birth day' do
-    visit('/')
     expect(page).to have_field('day')
   end
 
   scenario 'homepage has a field to enter your birth month' do
-    visit('/')
     expect(page).to have_field('month')
   end
 
   scenario 'homepage has a go button' do
-    visit('/')
     expect(page).to have_button('Go!')
   end
 end
